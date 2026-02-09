58ebb0d4920318cb30fedd3e330bc631a99d649a
maci-kb24 <macikb8@yahoo.com>
2026-02-09 18:09:54 +0100

    feat: register block components

---
+++ app/blocks/registry.ts
@@ -0,0 +1,9 @@
+import { ButtonBlock } from "./ButtonBlock";
+import { ImageBlock } from "./ImageBlock";
+import { TextBlock } from "./TextBlock";
+
+export const block_components = {
+  text: TextBlock,
+  button: ButtonBlock,
+  image: ImageBlock,
+};
