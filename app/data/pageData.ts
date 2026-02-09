5c34daf57dfc3061564ab0e792993fa2efcda97d
maci-kb24 <macikb8@yahoo.com>
2026-02-09 18:13:19 +0100

    update pagedata

--- app/data/pageData.ts
+++ app/data/pageData.ts
@@ -1,8 +1,20 @@
-export const pageData = {
+import { Block } from "../blocks/types";
+
+export const pageData: { blocks: Block[] } = {
   blocks: [
     {
       type: 'text',
-      props: { content: 'Hello world' }
+      props: { content: 'Welcome to my page!' }
+    },
+    {
+      type: 'button',
+      props: { label: 'Get started', href: 'https://example.com' }
+    },
+    {
+      type: 'image',
+      props: { src: 'https://placehold.co/400', alt: 'Example image', width: 500, height: 500 }
     }
   ]
 };
+
+
